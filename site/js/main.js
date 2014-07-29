ec169a5f182233af7d99c91ca15989dee194112e
Edward Longman <eialongman@gmail.com>
2014-07-29 22:05:15 +0100

    now gets all plugins from list and tries to display them

--- site/js/main.js
+++ site/js/main.js
@@ -1,7 +1,26 @@
 $(function(window,undefined){
+function appendPluginData(name){
 	$.post("api/get.php",
-		{plugin:"pluginTemplate"},
+		{plugin:name},
 		function(data){
-			$(".container").append(data.markup);
-		});
+			var markup=$("<div/>").addClass("plugin")
+				.append($("<h3/>").text(data.name))
+				.append(data.markup);
+			$(".container").append(markup);
+		},
+		'json'
+	);
+}
+function getAvaliablePlugins(){
+	$.post("api/list.php",
+		{},
+		function(data){
+			for(var i=0;i<data.length;i++){
+				appendPluginData(data[i]);
+			}
+		},
+		'json'
+	);
+}
+getAvaliablePlugins();
 });
