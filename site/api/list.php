1a5dfeee46e62dcbd0a244453c7c2d92ae78db39
Taiiwo Llort <tylerzacc@gmail.com>
2014-07-29 19:13:47 +0100

    Removes file extensions and ignores non php files.

--- site/api/list.php
+++ site/api/list.php
@@ -1,13 +1,16 @@
 <?php
 $list = scandir("../../plugins/");
 $list = array_slice($list, 2);
-for ( $i = 0; $i < $list.length; $i++){
+for ( $i = 0; $i < count($list); $i++){
 	//if the file/folder ends in ".php"
-	if (preg_match("/^\w{40}\.php$/", $list[$i])){
+	if (strpos($list[$i], ".php")){
 		$list[$i] = str_replace(".php", "", $list[$i]);
+	}
 	else {
 		//remove incorrectly named files from the list
-		unset($list[$i]);
+		array_splice($list, $i, 1);
+		//Ajust the iteration after key removal
+		$i -= 1;
 	}
 }
 echo json_encode($list);
