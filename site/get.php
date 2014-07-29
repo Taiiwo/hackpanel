c68d6f2b62a1e2c185da6991efa3e98ce79dbc6a
Taiiwo Llort <tylerzacc@gmail.com>
2014-07-29 15:43:03 +0100

    moved to api/

--- site/get.php
+++
@@ -1,7 +0,0 @@
-<?php
-//import required function
-$className = preg_replace("/\w{40}/", "", $_POST['className']);
-require_once "../plugins/$className.php";
-//run all class's main() function
-//echo ouput
-?>
