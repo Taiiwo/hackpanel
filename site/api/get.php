d7fb2f05825996ce4c956a393fa3bf04b5a4b131
Edward Longman <eialongman@gmail.com>
2014-07-29 21:07:08 +0100

    correct missing semi colon

--- site/api/get.php
+++ site/api/get.php
@@ -1,7 +1,7 @@
 <?php
 //import required function
 $className = preg_replace("/^\w{40}$/", "", $_POST['className']);
-$searchTerm = preg_replace("/^\w{140}$/", "", $_POST['searchTerm']);
+$searchTerm = preg_replace("^/\w{140}$/", "", $_POST['searchTerm']);
 require_once "../../plugins/" . $className . ".php";
 $thisClass = new $className;
 //run the class main() function
@@ -10,6 +10,6 @@ $pluginOutput = $thisClass->update($searchTerm);
 //echo ouput
 $returnObj=array("last_updated"=>time(),
 				"markup"=>$pluginOutput,
-				"scripts"=>$thisClass->scripts)
+				"scripts"=>$thisClass->scripts);
 echo json_encode($returnObj);
 ?>
