fe20f1d89de1278ffacbb5d5b0ef064084abea59
Taiiwo Llort <tylerzacc@gmail.com>
2014-07-29 18:16:49 +0100

    remove incorrectly names files from the list

--- site/api/list.php
+++ site/api/list.php
@@ -2,8 +2,13 @@
 $list = scandir("../../plugins/");
 $list = array_slice($list, 2);
 for ( $i = 0; $i < $list.length; $i++){
+	//if the file/folder ends in ".php"
 	if (preg_match("/^\w{40}\.php$/", $list[$i])){
-	$list[$i] = str_replace(".php", "", $list[$i]);
+		$list[$i] = str_replace(".php", "", $list[$i]);
+	else {
+		//remove incorrectly named files from the list
+		unset($list[$i]);
+	}
 }
 echo json_encode($list);
 ?>
