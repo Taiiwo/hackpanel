9d92053e7d10fcf00d3d16c190b819a460bcdb03
Taiiwo Llort <tylerzacc@gmail.com>
2014-07-29 22:12:21 +0100

    Catch all the errors and give back nice error messages. Also add support for new plugin parameters

--- site/api/get.php
+++ site/api/get.php
@@ -1,13 +1,23 @@
 <?php
-if (array_key_exists('searchTerm',$_POST)){
+if (array_key_exists('searchTerm',$_POST) && $_POST['searchTerm'] != ''){
 	$searchTerm = preg_replace("/^\w{140}$/", "", $_POST['searchTerm']);
 }
 else {
 	$searchTerm = "yrs";
 }
 //import required function
-$className = preg_replace("/^\w{40}$/", "", $_POST['plugin']);
-require_once "../../plugins/" . $className . ".php";
+if (array_key_exists('plugin',$_POST) && $_POST['plugin'] != ''){
+	$className = preg_replace("/^\w{40}$/", "", $_POST['plugin']);
+}
+else {
+	die("[E] invalid class name");
+}
+if (file_exists("../../plugins/" . $className . ".php")) {
+	require_once "../../plugins/" . $className . ".php";
+}
+else {
+	die("That plugin does not exist: $className.php");
+}
 $thisClass = new $className;
 //run the class main() function
 $pluginOutput = $thisClass->update($searchTerm);
@@ -16,6 +26,7 @@ $pluginOutput = $thisClass->update($searchTerm);
 $returnObj=array("last_updated"=>time(),
 				"update"=>$thisClass->update,
 				"markup"=>$pluginOutput,
+				"title"=>$thisClass->title,
 			//	"scripts"=>file_get_contents("../../plugins/js/$className.js"),
 				"scripts"=>$thisClass->scripts);
 echo json_encode($returnObj);
