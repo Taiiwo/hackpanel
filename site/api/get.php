19cdf60c20b6a365fdb8a87f311d0f8ed9a44463
Taiiwo Llort <tylerzacc@gmail.com>
2014-07-29 15:35:59 +0100

    change the string concatination for the plugin require

--- site/api/get.php
+++ site/api/get.php
@@ -2,10 +2,12 @@
 //import required function
 $className = preg_replace("/\w{40}/", "", $_POST['className']);
 $searchTerm = preg_replace("/\w{140}/", "", $_POST['searchTerm']);
-require_once "../plugins/$className.php";
-$thisClass = new $classname;
+require_once "../../plugins/" . $className . ".php";
+$thisClass = new $className;
 //run the class main() function
-$pluginOutput = $thisClass.main($searchTerm);
+print_r($thisClass);
+$pluginOutput = $thisClass->main($searchTerm);
+
 //echo ouput
 echo $pluginOutput;
 ?>
