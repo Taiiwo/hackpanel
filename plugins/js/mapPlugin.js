3008522017318a4a0dcd9e0886229da1d8afa36e
James Brooks <me@jamesbrks.com>
2014-07-30 21:24:17 +0100

    Added a Google Maps plugin
    Doesn’t work yet

---
+++ plugins/js/mapPlugin.js
@@ -0,0 +1,16 @@
+function initialize() {
+			var latLong = new google.maps.LatLng(50.375715, -4.139280);
+			var mapOptions = {
+				center: latLong,
+				zoom: 17
+			};
+			var map = new google.maps.Map(document.getElementById("map-canvas"),
+					mapOptions);
+
+			var marker = new google.maps.Marker({
+    		position: latLong,
+    		map: map,
+    		title:"Plymouth University"
+		});
+		}
+	google.maps.event.addDomListener(window, 'load', initialize);
