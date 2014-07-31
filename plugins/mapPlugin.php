33704023ab5d77fd5b21f15ac562472b3ca65ab7
Taiiwo Llort <tylerzacc@gmail.com>
2014-07-31 21:46:18 +0100

    Add marker example

--- plugins/mapPlugin.php
+++ plugins/mapPlugin.php
@@ -15,16 +15,22 @@ class mapPlugin {
 	//This function will be executed every time your plugin is updated.
 	function update($searchTerm){
 		return '<div id="map-canvas"/><script>
-var map;
 function initialize() {
+  var myLatlng = new google.maps.LatLng(-25.363882,131.044922);
   var mapOptions = {
-    zoom: 8,
+    zoom: 4,
     disableDefaultUI: true,
-    center: new google.maps.LatLng(-34.397, 150.644)
-  };
-  map = new google.maps.Map(document.getElementById(\'map-canvas\'),
-      mapOptions);
+    center: myLatlng
+  }
+  var map = new google.maps.Map(document.getElementById(\'map-canvas\'), mapOptions);
+
+  var marker = new google.maps.Marker({
+      position: myLatlng,
+      map: map,
+      title: \'Hello World!\'
+  });
 }
+
 initialize();
 </script>';
 	}
