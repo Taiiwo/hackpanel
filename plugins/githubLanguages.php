8040282801d67a43c3b75d4b788b5c9152970cb4
Taiiwo Llort <tylerzacc@gmail.com>
2014-07-31 02:07:13 +0100

    Add pie charts

--- plugins/githubLanguages.php
+++ plugins/githubLanguages.php
@@ -1,9 +1,18 @@
 <?php
 //This class MUST be called the same as the file name without ".php"
+function recursive_array_search($needle,$haystack) {
+    foreach($haystack as $key=>$value) {
+        $current_key=$key;
+        if($needle===$value OR (is_array($value) && recursive_array_search($needle,$value) !== false)) {
+            return $current_key;
+        }
+    }
+    return false;
+}
 class githubLanguages {
 	//A short description of your plugin
 	public $title = "Github";
-	public $scripts;
+	public $scripts = ["https://www.google.com/jsapi"];
 	//This sets whether you want the plugin to be continually updated.
 	public $update = false;
 	// This function is run once at plugin initialisation
@@ -25,24 +34,41 @@ class githubLanguages {
 		curl_close($ch);
 		//End messy curl
 		$array = json_decode($content);
-		$langCount = array();
+		$langCount = array(array("Language","Number of Projects"));
 		foreach ($array->items as $item) {
-			if ($item->language != NULL) {//[bug] also check the date they were created with ->created_at
-				if (array_key_exists($item->language, $langCount)){//if the language of the current item
+			if ($item->language != NULL) {//[suggestion] also check the date they were created with ->created_at
+				if (recursive_array_search($item->language, $langCount)){//if the language of the current item
 										//is already in the language counter
-					$langCount[$item->language] += 1;
+					//get current number of languages for this language
+					foreach ( $langCount as $i => $lang ){
+						if ($lang[0] == $item->language){
+							$index = $i;
+						}
+					}
+					//add one to the current number
+					$langCount[$index][1] = $langCount[$index][1] + 1;
 				}
 				else{
 					//append it to the array
-					$langCount[$item->language] = 1;
+					array_push($langCount, array($item->language, 1));
 				}
 			}
 		}
-		$toRet = "";
-		foreach ($langCount as $language => $count){
-			$toRet .= "$language: $count <br />";
-		}
-		return $toRet;
+		$arrayData = json_encode($langCount);
+		return '<div id="piechart" ></div>
+<script type="text/javascript">
+google.load("visualization", "1", {packages:["corechart"]});
+google.setOnLoadCallback(drawChart);
+function drawChart() {
+        var data = google.visualization.arrayToDataTable(' . $arrayData . ');
+        var options = {
+                title: \'Github projects with "yrs" in the name\'
+        };
+        var chart = new google.visualization.PieChart(document.getElementById(\'piechart\'));
+        chart.draw(data, options);
+}
+</script>
+';
 	}
 }
 ?>
