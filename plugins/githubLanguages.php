3d8dc93ed904a6b3208ee4f16bd8b293a2d5c067
Taiiwo Llort <tylerzacc@gmail.com>
2014-07-31 22:04:20 +0100

    remove spacing for css text-align

--- plugins/githubLanguages.php
+++ plugins/githubLanguages.php
@@ -62,7 +62,7 @@ function drawChart() {
         var data = google.visualization.arrayToDataTable('. $arrayData .');
 
         var options = {
-	  chartArea: {left: 10, right: 10,  width: "90%", height: "90%", top: 10, bottom: 10},
+	  chartArea: {left: 0, right: 0,  width: "100%", height: "95%", top: 0, bottom: 0},
 	  legend: \'none\',//Omit this line and re-add the next two to put the legends back
 		/*
 	  legend: {textStyle: {color: "white", fontSize: 14}},
