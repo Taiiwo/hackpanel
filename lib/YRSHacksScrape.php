0b3d6c6d492b5460f034bd861aa88d0b048ba634
Edward Longman <eialongman@gmail.com>
2014-08-02 02:06:47 +0100

    stop using databases and store in files

--- lib/YRSHacksScrape.php
+++ lib/YRSHacksScrape.php
@@ -6,9 +6,8 @@ function loadYRSHacks(){
 	$page_dom=file_get_html("http://hacks.youngrewiredstate.org/events/FOC2014");
 	///ul/li/div[starts-with(@class,'image')/a/@href]
 	$projects_links=$page_dom->find("div#projects ul li div.image a");
-	global $db;
 	$quit=0;
-	$db_obj = new mysqli($db["host"],$db["user"],$db["pass"],$db["db"]);
+	$allUrls=[];
 	foreach($projects_links as $link){
 		$href=$link->href;
 		$project_url="http://hacks.youngrewiredstate.org".$href;
@@ -25,36 +24,31 @@ function loadYRSHacks(){
 			$i++;
 			$git_user=(count($url_parts)>$i)?$url_parts[$i]:null;
 			$i++;
-			$git_name=(count($url_parts)>$i)?$url_parts[$i]:null;
+			$git_repo=(count($url_parts)>$i)?$url_parts[$i]:null;
 			//echo $href.','.$git_location.','.$git_user.','.$git_name;
 			//insert the git info into the local git db
-			$result=$db_obj->query("SELECT `git_location` FROM `git` WHERE `git_location`='".$git_location."'");
-			if($result!==FALSE & $result->num_rows>0){
-				//echo "updating";
-				$db_obj->query("UPDATE `git` SET `url`='".$href."', 
-												 `git_location`=$git_location, 
-												 `git_user`=$git_name, 
-												 `git_name`=$git_user
-							WHERE `git_location`=$git_location");
+			foreach($allUrls as $url){
+				if($url["git_location"]==$git_location)continue;
 			}
-			else{
-				//echo "inserting";
-				$query="INSERT INTO `git` SET `url`='".$href."', 
-											  `git_location`='".$git_location."', 
-											  `git_user`='".$git_name."', 
-											  `git_name`='".$git_user."'";
-				$insert=$db_obj->query($query);
-			}
+			$allUrls[]=array(
+				"href"=>$href,
+				"git_location"=>$git_location,
+				"git_user"=>$git_user,
+				"git_repo"=>$git_repo
+			);
 		}
 		//echo $link->href;
 	}
+	$jsonFile=fopen('githubUrls.json', 'w');
+	fwrite($jsonFile,json_encode($allUrls));
+	fclose($jsonFile);
 }
 
 //this function compares two commits to see which one is more recent
 function commitCompare($num1,$num2){
 	$date1=new DateTime($num1->commit->author->date);
 	$date2=new DateTime($num2->commit->author->date);
-	if($date1>$date2){
+	if($date1<$date2){
 		//echo 'bigger';
 		return 1;
 	}
@@ -85,24 +79,21 @@ function getRepoCommits($user,$repo){
 }
 
 function loadGithubCommits(){
-	global $db,$gitOAuth;
-	$db_obj = new mysqli($db["host"],$db["user"],$db["pass"],$db["db"]);
-	$projects = $db_obj->query("SELECT `git_user`,`git_name` FROM `git` WHERE `git_user`!='' AND `git_name`!=''");
+	global $gitOAuth;
+	$projects=json_decode(file_get_contents("githubUrls.json"),true);
 	$allCommits=array();
-	if($projects!==FALSE & $projects->num_rows>0){
-		$projectsArray=$projects->fetch_all(MYSQL_ASSOC);
-		foreach ($projectsArray as $project){
-			$content=getRepoCommits($project["git_name"],$project["git_user"]);
-			$commits = json_decode($content);
-			if(is_array($commits)){
-				for($i=0;$i<count($commits)&$i<30;$i++){
-					$allCommits[]=$commits[$i];
-				}
+	foreach ($projects as $project){
+		if(count($project)<4)continue;
+		$content = getRepoCommits($project["git_user"],$project["git_repo"]);
+		$commits = json_decode($content);
+		if(is_array($commits)){
+			for($i=0;$i<count($commits)&$i<30;$i++){
+				$allCommits[]=$commits[$i];
 			}
-			echo "<br/>".$project["git_name"]."/".$project["git_user"];
 		}
-		usort($allCommits,"commitCompare");
+		echo "<br/>".$project["git_user"]."/".$project["git_repo"];
 	}
+	usort($allCommits,"commitCompare");
 	?><ul><?php
 	$limiter=0;
 	foreach($allCommits as $result){
@@ -115,13 +106,14 @@ function loadGithubCommits(){
 		$i++;$i++;
 		$git_user=(count($url_parts)>$i)?$url_parts[$i]:null;
 		$i++;
-		$git_name=(count($url_parts)>$i)?$url_parts[$i]:null;
-		echo "<li>".$result->commit->author->name." COMMITTED ".$result->commit->message." ON ".$git_user."/".$git_name."</li>";
+		$git_repo=(count($url_parts)>$i)?$url_parts[$i]:null;
+		echo "<li>".$result->commit->author->name." COMMITTED ".$result->commit->message." ON ".$git_user."/".$git_repo." AT ".$result->commit->author->date."</li>";
 		if($limiter>100)break;
 		$limiter++;
 	}
 	?></ul><?php
 	$jsonFile=fopen('githubCommits.json', 'w');
-	fwrite($jsonFile,json_encode($allCommits));
+	$topCommits=array_chunk($allCommits,100);
+	fwrite($jsonFile,json_encode($topCommits[0]));
 	fclose($jsonFile);
 }
