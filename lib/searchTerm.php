8033b10ad53262b2e0e98b1d3221e71b56e8b4d4
Taiiwo Llort <tylerzacc@gmail.com>
2014-08-09 23:35:53 +0100

    change die to return false and add fallback function for if data is not in the json obj

--- lib/searchTerm.php
+++ lib/searchTerm.php
@@ -1,6 +1,6 @@
 <?php
 function getSearchAlias($term,$keys){
-	if(!file_exists("../../site/api/searches.json"))die("couldn't find json");
+	if(!file_exists("../../site/api/searches.json"))return false;
 	$hackathonsJSON=file_get_contents("../../site/api/searches.json");
 	$hackathons=json_decode($hackathonsJSON,true);
 	foreach ($hackathons as $hackathon){
@@ -19,3 +19,15 @@ function getSearchAlias($term,$keys){
 	}
 	return $returnArray;
 }
+function getEventDetails(searchTerm) {
+	getSearchAlias
+	//api url
+	$url = "" . searchTerm;
+	//get json data
+	eventsJson = json_decode(file_get_contents($url));
+	retme = array();
+	retme['irc'] = eventsJson->ircChannel;
+	//etc
+	return retme;
+}
+?>
