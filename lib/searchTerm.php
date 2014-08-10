9b7eb573829d141633c6219b4b93388ae617a606
Taiiwo Llort <tylerzacc@gmail.com>
2014-08-10 01:06:49 +0100

    Will now attempt to find information on an event if it is not present in the searches.json file. Seriously, take a look at this shit, I'm pretty proud of this one.

--- lib/searchTerm.php
+++ lib/searchTerm.php
@@ -1,11 +1,27 @@
 <?php
 function getSearchAlias($term,$keys){
+	//check if searches file exists
 	if(!file_exists("../../site/api/searches.json"))return false;
 	$hackathonsJSON=file_get_contents("../../site/api/searches.json");
 	$hackathons=json_decode($hackathonsJSON,true);
+	$found = false;
+	//see if we have the hackathon already
 	foreach ($hackathons as $hackathon){
 		if ($hackathon["default"]==$term){
+			$found = true;
 			break;
+		}
+	}
+	//If we haven't got it
+	if (!$found) {
+		//We don't have information on that hackathon yet, so let's search for it!
+		$hackathon = getEventDetails($term);
+		if ($hackathon) {
+			//put some stuff here to add $hackathon to searches.json
+		}
+		else {
+			//no results found, die
+			return false;
 		}
 	}
 	$returnArray = array();
@@ -19,15 +35,23 @@ function getSearchAlias($term,$keys){
 	}
 	return $returnArray;
 }
-function getEventDetails(searchTerm) {
-	getSearchAlias
+function getEventDetails($searchTerm) {
 	//api url
-	$url = "" . searchTerm;
-	//get json data
-	eventsJson = json_decode(file_get_contents($url));
-	retme = array();
-	retme['irc'] = eventsJson->ircChannel;
-	//etc
-	return retme;
+	$apiKey = "kMvCXkjNRCk7X787";
+	$url = "http://api.eventful.com/rest/events/search?app_key=$apiKey&keywords=$searchTerm";
+	$events = simplexml_load_file($url);
+	if (!count($events) > 0){
+		//no results
+		return false;
+	}
+	$events = $events->events->event[0];
+	$retme = array();
+	$retme["default"] = $searchTerm;
+	$retme["lat"] = $events->latitude;
+	$retme["lng"] = $events->longitude;
+	$retme["start-date"] = $events->start_time;
+	$retme["end-date"] = $events->stop_time;
+	$retme["description"] = $events->description;
+	return $retme;
 }
 ?>
