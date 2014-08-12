309bc6e32d3fa134461ae42e8d02ded19489b241
Taiiwo Llort <tylerzacc@gmail.com>
2014-08-12 22:41:13 +0100

    Change the dimensions of it to two wide because it looks better IMO, but I was only using it to test

--- plugins/mapPlugin.php
+++ plugins/mapPlugin.php
@@ -4,6 +4,7 @@ class mapPlugin {
 	//A short description of your plugin
 	public $title = "Map Plugin";
 	public $scripts = array();
+	public $size = array(2, 1);
 	//This sets whether you want the plugin to be continually updated.
 	public $update = false;
 	// This function is run once at plugin initialisation
