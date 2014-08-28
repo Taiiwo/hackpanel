a336816003457d65ae76189b831c1da3be7a254e
James Brooks <james.brooks2@btinternet.com>
2014-08-28 11:19:22 +0100

    OK - re-implemented my new form, except it DEFINITELY isn't broken this time.

--- site/js/lightboxes.js
+++ site/js/lightboxes.js
@@ -1,4 +1,4 @@
-$(document).ready(function(d) {
+$(document).ready(function() {
     $('#add-button').click(function() {
       $('#add').fadeIn();
     })
@@ -13,5 +13,36 @@ $(document).ready(function(d) {
 
     $('#settingsClose').click(function() {
       $('#settings').fadeOut();
+    })
+
+    $('#eventName').on("keyup", function (e) {
+      var removeSpaces = $('#eventName').val().replace(/\s/g,"")
+      var removePunctuation = removeSpaces.replace(/[\.,-\/#!$%\^&\*;:{}£€=\-_`~()@\+\?><\[\]\+]/g, '')
+      $('#eventURL').val(removePunctuation);
+      var length = this.value.length;
+      var remain = 30 - length;
+      $('.nameRemaining').text(remain + " characters remaining");
+    })
+
+    $('#eventURL').on("keypress", function(e) {
+      if (e.keyCode == 32) {
+        return false;
+      }
+    })
+
+    $('#eventDescription').on("keyup", function(e) {
+      var length = this.value.length;
+      var remain = 350 - length
+      $('.descRemaining').text(remain + " characters remaining");
+    })
+
+    $('#imageButton').click(function(e) {
+      e.preventDefault();
+      $('#imageInput').click();
+      $('#imageInput').change(function() {
+        var value = this.value;
+        var fileName = typeof value == 'string' ? value.match(/[^\/\\]+$/)[0] : value[0]
+        $('.imagePath').text(fileName)
+      })
     })
 });
